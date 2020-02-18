f8370d48552012d5ee52e7b43d2a94b38b50b646
Andreas von Brandis <andreasvbrandis@gmail.com>
2020-02-18 11:57:10 +0100

    la til funskjoner i door logikk

--- source/Door_logic.c
+++ source/Door_logic.c
@@ -1,3 +1,10 @@
+
+#include "studio.h"
 #include "hardware.h"
 #include "door_logic.h"
 #include "timer.h"
+
+
+int door_state(int state){
+
+}
---
+++ source/Door_logic.h
@@ -0,0 +1,17 @@
+
+
+
+typdef enum {
+   Door_open,
+   Door_close
+} state;
+
+
+
+int door_state(int door_state);
+
+void open_door();
+
+void close_door();
+
+void Check_obstruction();
