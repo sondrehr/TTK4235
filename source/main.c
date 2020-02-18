02d31dee86dec8e1775d326b22436ac69306023d
Sondre H. Rognlien <sondreholth98@gmail.com>
2020-02-18 10:46:14 +0100

    Endret stopp knapp

--- source/main.c
+++ source/main.c
@@ -40,9 +40,8 @@ int main(){
     hardware_command_movement(HARDWARE_MOVEMENT_UP);
 
     while(1){
-        if(hardware_read_stop_signal()){
+        while(hardware_read_stop_signal()){
             hardware_command_movement(HARDWARE_MOVEMENT_STOP);
-            break;
         }
 
         if(hardware_read_floor_sensor(0)){
