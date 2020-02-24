19529d289b20f8a8fa35ac6de5fadd9abaa4b978
Simen S. Røstad <simen.rostad@nordicsemi.no>
2020-02-24 12:11:47 +0100

    løk

--- source/Door_logic.c
+++ source/Door_logic.c
@@ -6,7 +6,6 @@
 
 
 
-<<<<<<< HEAD
 void door_logic(int *inside, int *up, int *down, int *next, int current_floor, int last_direction){
 	
 	hardware_command_movement(HARDWARE_MOVEMENT_STOP);
@@ -33,16 +32,6 @@ void door_logic(int *inside, int *up, int *down, int *next, int current_floor, i
         update_queue(next, current_floor);
         update_lights_and_orders(current_floor-1, inside, up, down);
 	};
-=======
-//int door_state(state tilstand){
-//    return tilstand;
-//}
-
-void door_logic(){
-	start_timer();
-	hardware_command_door_open(1);
-	while(read_timer(3) || hardware_read_obstruction_signal() || hardware_read_stop_signal()){};
->>>>>>> 1b2e94ee2f2ef172bc010ad48dd737f1f54c6ad1
 	hardware_command_door_open(0);
 }
 
