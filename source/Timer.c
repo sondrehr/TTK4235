8fea447198648dd883a8af5452b1cf6755e0169b
Andreas von Brandis <andreasvbrandis@gmail.com>
2020-02-24 09:10:42 +0100

    Håper timer funker nå

--- source/Timer.c
+++ source/Timer.c
@@ -3,9 +3,11 @@
 #include <stdio.h>
 #include <time.h>
 
-int pre_value;
+//int pre_value;
+time_t pre_value;
 
-int end_value;
+//int end_value;
+time_t end_value;
 
 void start_timer(){
   time(&pre_value); //gir sekunder siden 1 jan 1970
@@ -16,7 +18,7 @@ int read_timer(int seconds) {
 
   time(&end_value);
 
-  if (difftime(end_value, prevalue) <= seconds) {
+  if (difftime(end_value, pre_value) <= seconds) {
   return 1}
 
   else {
